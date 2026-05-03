8e151d044a4e36392a7b8866b093ebf0e853da7d
imranafset <mi3081557@gmail.com>
2026-05-03 15:04:36 +0400

    adedd sql data

---
+++ 03-ecommerce-app/backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, description, price) VALUES ('Gaming Mouse', 'RGB Lighting', 50.00);
+INSERT INTO product (name, description, price) VALUES ('Mechanical Keyboard', 'Blue Switches', 100.00);
+INSERT INTO product (name, description, price) VALUES ('HD Monitor', '27 inch, 144Hz', 300.00);
+INSERT INTO product (name, description, price) VALUES ('USB-C Hub', '4 Ports', 25.00);
+INSERT INTO product (name, description, price) VALUES ('External SSD', '1TB, USB 3.1', 150.00);
