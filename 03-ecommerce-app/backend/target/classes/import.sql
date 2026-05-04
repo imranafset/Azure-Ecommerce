f4d6e2cca7105cd81973c574e26b42e410547b34
imranafset <mi3081557@gmail.com>
2026-05-04 16:54:52 +0400

    build new tag modified in sql file

--- 03-ecommerce-app/backend/target/classes/import.sql
+++ 03-ecommerce-app/backend/target/classes/import.sql
@@ -1,2 +1,1 @@
-INSERT INTO product (name, description, price) VALUES ('Gaming Mouse', 'RGB Lighting', 50.00);
-INSERT INTO product (name, description, price) VALUES ('Mechanical Keyboard', 'Blue Switches', 100.00);
+INSERT INTO product (name, description, price) VALUES ('Gaming Mouse', 'RGB', 50.00);
